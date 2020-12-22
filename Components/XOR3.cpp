ab76a0f4eef9c2f924bc1fae6c62ab036ddae241
Mahmed amer <67114727+Mahmedamer@users.noreply.github.com>
2020-12-22 20:34:20 +0200

    Added XOR3 Component Class and a little change in NOR3 Class

--- Components/XOR3.cpp
+++ Components/XOR3.cpp
@@ -1,6 +1,6 @@
-#include "NOR3.h"
+#include "XOR3.h"
 
-NOR3::NOR3(const GraphicsInfo& r_GfxInfo, int r_FanOut) :Gate(2, r_FanOut)
+XOR3::XOR3(const GraphicsInfo& r_GfxInfo, int r_FanOut) :Gate(2, r_FanOut)
 {
 	m_GfxInfo.x1 = r_GfxInfo.x1;
 	m_GfxInfo.y1 = r_GfxInfo.y1;
@@ -9,7 +9,7 @@ NOR3::NOR3(const GraphicsInfo& r_GfxInfo, int r_FanOut) :Gate(2, r_FanOut)
 }
 
 
-void NOR3::Operate()
+void XOR3::Operate()
 {
 	//caclulate the output status as the ANDing of the two input pins
 	//Add you code here
@@ -17,7 +17,8 @@ void NOR3::Operate()
 	Inputs[0] = GetInputPinStatus(1);
 	Inputs[1] = GetInputPinStatus(2);
 	Inputs[2] = GetInputPinStatus(3);
-	if (!Inputs[0] && !Inputs[1] && !Inputs[2])
+	int ones = Inputs[0] + Inputs[1] + Inputs[2];
+	if (ones % 2)
 	{
 		m_OutputPin.setStatus(HIGH);
 	}
@@ -29,28 +30,28 @@ void NOR3::Operate()
 
 
 // Function Draw
-// Draws 3-input NOR gate
-void NOR3::Draw(Output* pOut)
+// Draws 3-input XOR3 gate
+void XOR3::Draw(Output* pOut)
 {
 	//Call output class and pass gate drawing info to it.
-	pOut->DrawNOR3(m_GfxInfo);
+	pOut->DrawXOR3(m_GfxInfo);
 }
 
 //returns status of outputpin
-int NOR3::GetOutPinStatus()
+int XOR3::GetOutPinStatus()
 {
 	return m_OutputPin.getStatus();
 }
 
 
 //returns status of Inputpin #n
-int NOR3::GetInputPinStatus(int n)
+int XOR3::GetInputPinStatus(int n)
 {
 	return m_InputPins[n - 1].getStatus();	//n starts from 1 but array index starts from 0.
 }
 
 //Set status of an input pin ot HIGH or LOW
-void NOR3::setInputPinStatus(int n, STATUS s)
+void XOR3::setInputPinStatus(int n, STATUS s)
 {
 	m_InputPins[n - 1].setStatus(s);
 }
